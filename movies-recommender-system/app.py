25456dfab925c1d9515db3f6c35b1a57d299b45b
Najafizadeah <A.latifnajafizadeh@gmil.com>
2023-10-26 20:17:52 +0330

    endpoint

--- movies-recommender-system/app.py
+++ movies-recommender-system/app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pickle
 import pandas as pd
 import streamlit as st
@@ -52,9 +51,6 @@ if st.button('Recommend'):
     with col5:
         st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
-=======
-import streamlit as st
 
-st.title('movie recommender system')
 
->>>>>>> dc6f35a9e47ab47b247ce8426a626f96e347947a
+
